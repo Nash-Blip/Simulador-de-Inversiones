3f3e156195891ff3a020d44e12368ed0c58fcbc2
begueagustinhernan <begueagustinhernan@gmail.com>
2026-05-21 14:55:15 -0300

    feat: Agrego campos nuevos {email, password, rol} necesarios para autenticar al inversor (usuario del sistema)

--- backend/src/inversor/dto/create-inversor.dto.ts
+++ backend/src/inversor/dto/create-inversor.dto.ts
@@ -1,8 +1,20 @@
-import { IsNumber, IsString, ValidateNested } from "class-validator";
+import { IsEmail, IsNumber, IsString, IsEnum, IsOptional, MinLength } from "class-validator";
+import { InversorRol } from "../entities/inversor.entity";
 
 export class CreateInversorDto {
+    @IsEmail()
+    email!: string;
+
     @IsString()
     nombre!: string;
+
+    @IsString()
+    @MinLength(6)
+    password!: string;
+
+    @IsOptional()
+    @IsEnum(InversorRol)
+    rol?: InversorRol;
 
     @IsNumber()
     saldoVirtual!: number;
