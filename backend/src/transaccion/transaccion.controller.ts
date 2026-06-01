2cbcce074fdf747f9b1f193a9177f3291bcc008d
begueagustinhernan <begueagustinhernan@gmail.com>
2026-06-01 13:45:53 -0300

    feat(transaccion): implemento paginacion fija y filtros dinamicos
    Ahora tenemos:
    un findAll(), unicamente ejecutable con permisos de administrador, que trae todas las transacciones
    
    findHistorialTransacciones(), ejecutable con permisos de usuario, que trae todas las transacciones del inversor autenticado.
    
    Ambos metodos, paginados en páginas de a 10 registros y agregando filtros de busqueda que viajan en la URL

--- backend/src/transaccion/transaccion.controller.ts
+++ backend/src/transaccion/transaccion.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, UseGuards} from "@nestjs/common";
+import { Controller, Get, UseGuards, Req, Query } from "@nestjs/common";
+import type { Request } from 'express';
 import { TransaccionService } from "./transaccion.service";
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/auth/guards/roles.guard';
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { InversorRol } from '@/inversor/entities/inversor.entity';
+import { GetTransaccionesQueryDto } from "./dto/get-transaccion-query.dto";
 
 @Controller('transaccion')
 export class TransaccionController {
     constructor(private readonly transaccionService: TransaccionService) { }
 
     @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(InversorRol.ADMIN)
+    findAll(@Query() query: GetTransaccionesQueryDto) {
+        return this.transaccionService.findAll(query);
+    }
+
+    @Get('historial')
     @UseGuards(JwtAuthGuard)
-    findAll() {
-        return this.transaccionService.findAll();
+    findHistorial(@Query() query: GetTransaccionesQueryDto, @Req() req: Request) {
+        return this.transaccionService.findHistorialTransacciones((req as any).user.id, query);
     }
 }
+
