1a9cc8cee484c4906dfa228d434eb835d5c1908f
AlejoSuarez <alejosuarez731@gmail.com>
2026-06-04 21:01:58 -0300

    implementación tests activo

--- backend/src/inversor/inversor.service.spec.ts
+++ backend/src/inversor/inversor.service.spec.ts
@@ -1,18 +1,1 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { InversorService } from './inversor.service';
 
-describe('InversorService', () => {
-  let service: InversorService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [InversorService],
-    }).compile();
-
-    service = module.get<InversorService>(InversorService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
