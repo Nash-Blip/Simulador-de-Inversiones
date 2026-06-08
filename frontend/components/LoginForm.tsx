2956ad789958dcd5daeb9b1dfe106020fd023dbf
Ramiro Gómez Rivelli <gomezrivelliramiro@gmail.com>
2026-06-08 18:36:47 -0300

    Update LoginForm.tsx

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
-    const router = useRouter();
 
     function handleSubmit(e: React.SyntheticEvent) {
         e.preventDefault();
