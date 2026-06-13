f090052cdb129ddf679e9690c78ed895e79c68be
begueagustinhernan <begueagustinhernan@gmail.com>
2026-06-13 12:36:52 -0300

    feat: Configuro SwaggerModule en main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({  
+  app.enableCors({
     origin: 'http://localhost:3001',
-    credentials: true,});
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,6 +19,17 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Simulador de Inversiones API')
+    .setDescription('API del simulador de inversiones')
+    .setVersion('1.0')
+    .addCookieAuth('token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
