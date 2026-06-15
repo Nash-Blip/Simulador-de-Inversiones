4f3b3b1d5d7ef586a921dc9be7b337e78951855e
Matias Fernandez <dev.mattt@gmail.com>
2026-06-14 21:21:10 -0300

    Defino pagina principal y componentes

--- frontend/components/FeaturesInicio.tsx
+++ frontend/components/FeaturesInicio.tsx
@@ -1,15 +1,15 @@
 export default function FeaturesInicio() {
     return (
-        <section className="bg-white dark:bg-gray-900">
-            <div className="container px-6 py-10 mx-auto">
-                <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+        <section className=" bg-darkblue w-full ">
+            <div className="container px-6 py-10 mx-auto my-12">
+                <h1 className="text-2xl font-semibold text-center text-gray-800  lg:text-3xl dark:text-white">
                     Explora las herramientas del <span className="text-blue-500">Simulador</span>
                 </h1>
                 <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
                         <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fill-rule="evenodd" d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.04 16.5.5-1.5h6.42l.5 1.5H8.29Zm7.46-12a.75.75 0 0 0-1.5 0v6a.75.75 0 0 0 1.5 0v-6Zm-3 2.25a.75.75 0 0 0-1.5 0v3.75a.75.75 0 0 0 1.5 0V9Zm-3 2.25a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0v-1.5Z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.04 16.5.5-1.5h6.42l.5 1.5H8.29Zm7.46-12a.75.75 0 0 0-1.5 0v6a.75.75 0 0 0 1.5 0v-6Zm-3 2.25a.75.75 0 0 0-1.5 0v3.75a.75.75 0 0 0 1.5 0V9Zm-3 2.25a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0v-1.5Z" clipRule="evenodd" />
                             </svg>
                         </span>
                         <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">Proyecciones en Tiempo Real</h1>
@@ -21,7 +21,7 @@ export default function FeaturesInicio() {
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
                         <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fill-rule="evenodd" d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
                                 <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
                             </svg>
                         </span>
@@ -34,7 +34,7 @@ export default function FeaturesInicio() {
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
                         <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clipRule="evenodd" />
                             </svg>
                         </span>
                         <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">Entorno 100% Seguro</h1>
---
+++ frontend/components/PreguntasFrecuentes.tsx
@@ -0,0 +1,112 @@
+export default function PreguntasFrecuentes() {
+    return (
+        <>
+            <section className="bg-white dark:bg-gray-900 w-full">
+                <div className="container px-6 py-4 mx-auto">
+                    <hr className="my-6 border-gray-200 md:my-10 dark:border-gray-700 w-2/3 mx-auto" />
+
+                    <h1 className="text-2xl font-semibold text-gray-800 lg:text-3xl dark:text-white text-center">FAQ - Preguntas Frecuentes</h1>
+
+                    <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-3">
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Cuales son los costos?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    No hay ningun tipo de costos. El simulador es gratuito y viene fondeado con saldo digital para que puedas utilizar.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Puedo operar sin tener experiencia?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    Si. Lo importante es estar siempre informado y utilizar el simulador como una herramienta de aprendizaje.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Necesito cumplir algun requisito para operar?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    Todas las personas que quieran operar usando el simulador, deben ser mayores de 18 años.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Que instrumentos se pueden operar?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    Actualmente, solo se pueden operar acciones de empresas nacionales e internacionales.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Puedo ingresar dinero?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    Si. El sistema te permite ingresar dinero ficticio simulando transferencias bancarias o pagos con tarjeta.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="inline-block p-3 text-white bg-blue-600 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </div>
+
+                            <div>
+                                <h1 className="text-xl font-semibold text-gray-700 dark:text-white">Hay algun registro de mis operaciones?</h1>
+
+                                <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
+                                    En la seccion de "Transacciones" podras ver todas tus operaciones realizadas.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+
+    )
+}
