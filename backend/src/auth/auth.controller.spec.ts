a1d13be6ebdd8af2e11b3b07d8c5f06a4368d28b
AlejoSuarez <alejosuarez731@gmail.com>
2026-06-16 19:52:17 -0300

    actualizo diagrama de clases

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { CreateInversorDto } from '@/inversor/dto/create-inversor.dto';
+import { CreateInversorDto } from '@/inversor/dto/input/create-inversor.dto';
 import type { Response } from 'express';
 
 describe('AuthController', () => {
