ba3ef5c3d8e6dd73cdc8097a31157b29003dfb28
Matias Fernandez <dev.mattt@gmail.com>
2026-06-23 22:42:44 -0300

    Uso de authcontext para paginas de Admin

--- frontend/app/(administrador)/admin/inversores/page.tsx
+++ frontend/app/(administrador)/admin/inversores/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+import { useAuth } from "@/app/auth/AuthContext";
 import ListInversores from "@/components/ListInversores";
+import { useEffect } from "react";
 
-export default function ListaInversoresPage(){
+export default function ListaInversoresPage() {
+            const { verificarSesion, loading } = useAuth();
+        
+            useEffect(() => {
+                verificarSesion();
+            }, [verificarSesion]);
+        
+            if (loading) {
+                return <p className="text-center mt-10 text-gray-400 animate-pulse">Cargando...</p>;
+            }
     return <ListInversores/>
 }
--- frontend/app/(administrador)/admin/transacciones/page.tsx
+++ frontend/app/(administrador)/admin/transacciones/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+import { useAuth } from "@/app/auth/AuthContext";
 import ListTransacciones from "@/components/ListTransacciones";
+import { useEffect } from "react";
 
 export default function ListTransaccionesPage() {
+            const { verificarSesion, loading } = useAuth();
+        
+            useEffect(() => {
+                verificarSesion();
+            }, [verificarSesion]);
+        
+            if (loading) {
+                return <p className="text-center mt-10 text-gray-400 animate-pulse">Cargando...</p>;
+            }
     return <ListTransacciones/>
 }
